ee3896e889f8d17bd5cc693848e7c9f1198b4dce
albino.nuta.costa <albino.nuta.costa@fidelidade.pt>
2021-12-20 07:03:41 +0000

    second commit

--- APIValidation/Steps/BaseApiScenariosSteps.cs
+++ APIValidation/Steps/BaseApiScenariosSteps.cs
@@ -1,9 +1,5 @@
 ﻿using APIValidation.Apis;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
 namespace APIValidation.Steps
